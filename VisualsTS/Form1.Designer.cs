1b78546db6bd94fc66fa786bd54a01609dddc42a
pandey2000 <pandey.aman369@gmail.com>
2020-07-01 12:43:59 +0530

    New code

---
+++ VisualsTS/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace VisualsTS
+{
+    partial class AnimationTS
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-5, 364);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(125, 37);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(126, 364);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(60, 37);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.richTextBox3.Location = new System.Drawing.Point(872, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(25, 25);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.richTextBox4.Location = new System.Drawing.Point(872, 43);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(25, 25);
+            this.richTextBox4.TabIndex = 2;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.richTextBox5.Location = new System.Drawing.Point(872, 74);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(25, 25);
+            this.richTextBox5.TabIndex = 2;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(903, 12);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(100, 25);
+            this.richTextBox6.TabIndex = 1;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(903, 43);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(100, 25);
+            this.richTextBox7.TabIndex = 1;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.Location = new System.Drawing.Point(903, 74);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(120, 25);
+            this.richTextBox8.TabIndex = 1;
+            this.richTextBox8.Text = "";
+            // 
+            // AnimationTS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 423);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "AnimationTS";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+    }
+}
---
+++ VisualsTS/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualsTS
+{
+    public partial class AnimationTS : Form
+    {
+        /// <summary>
+        /// Button upon clicking which the visualization starts.
+        /// </summary>
+        Button btn;
+
+        /// <summary>
+        /// Input to the csv file is provided here.
+        /// </summary>
+        RichTextBox inputPath;
+
+        /// <summary>
+        /// It stores the created tenant shards(shown by a square).
+        /// </summary>
+        List<RichTextBox> tenantShards = new List<RichTextBox>();
+
+        /// <summary>
+        /// The number of minutes simulation was ruun.
+        /// </summary>
+        int numberOfMinutes = 0;
+
+        /// <summary>
+        /// Stores data for every TS wether it contains fresh data or stale data at that time.
+        /// </summary>
+        string[,] data;
+
+        public AnimationTS()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Seconds:";
+            richTextBox2.Text = "0";
+            richTextBox3.BackColor = Color.Black;
+            richTextBox4.BackColor = Color.Red;
+            richTextBox5.BackColor = Color.Green;
+            richTextBox6.Text = "Stale/No data";
+            richTextBox7.Text = "Fresh data";
+            richTextBox8.Text = "Simulation over";
+            btn = new Button();
+            btn.Text = "Click here";
+            btn.Height = 40;
+            btn.Width = 60;
+            btn.Left = 700;
+            inputPath = new RichTextBox();
+            inputPath.Height = 40;
+            inputPath.Width = 500;
+            inputPath.Left = 200;
+            inputPath.Text = "Enter location of file";
+            inputPath.Enter += new EventHandler(inputPath_Enter);
+            inputPath.Leave += new EventHandler(inputPath_Leave);
+            this.Controls.Add(btn);
+            this.Controls.Add(inputPath);
+            btn.Click += new EventHandler(button1_Click);
+        }
+
+        private void inputPath_Enter(object sender, EventArgs e)
+        {
+            if (inputPath.Text == "Enter location of file")
+            {
+                inputPath.Text = "";
+            }
+        }
+
+        private void inputPath_Leave(object sender, EventArgs e)
+        {
+            if (inputPath.Text == "")
+            {
+                inputPath.Text = "Enter location of file";
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string path;
+            path = inputPath.Text;
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+                string[] col = lines[1].Split(',');
+                int numberOfTS = Convert.ToInt32(col[4]);   //col[4] contains number of tenant shards.
+                numberOfMinutes = Convert.ToInt32(col[9]) + 1;  //col[9] contains the config parameter: number of minutes
+                int fanout = Convert.ToInt32(col[24]); //col[24] contains config param: fanout
+                data = new string[numberOfTS, numberOfMinutes * 6];
+                int flag = 0;
+                foreach (string line in lines)
+                {
+                    if (flag == 0)
+                    {
+                        flag = 1;
+                        continue;
+                    }
+                    string[] columns = line.Split(',');
+                    data[Convert.ToInt32(columns[2]), Convert.ToInt32(columns[1])] = columns[3];
+                }
+
+                int numberOfLevels = 1;
+                int totalTSTillThisLevel = 1;
+                while (totalTSTillThisLevel < numberOfTS)
+                {
+                    totalTSTillThisLevel += (int)Math.Pow(fanout, numberOfLevels);
+                    numberOfLevels++;
+                }
+
+                int currentTS = 1;
+                for (int i = 0; i < numberOfLevels; i++)
+                {
+                    int numberOfTSAtThisLevel = (int)Math.Pow(fanout, i);
+                    int currentNumberOfTSAtThisLevel = 0;
+                    while (currentTS <= numberOfTS && currentNumberOfTSAtThisLevel < numberOfTSAtThisLevel)
+                    {
+                        RichTextBox r = new RichTextBox();
+                        r.Height = 20;
+                        r.Width = 20;
+                        r.Left = currentNumberOfTSAtThisLevel * 21;
+                        r.Top = (i + 1) * 21;
+                        tenantShards.Add(r);
+                        this.Controls.Add(r);
+                        currentTS++;
+                        currentNumberOfTSAtThisLevel++;
+                    }
+                }
+                Thread t = new Thread(new ThreadStart(SayHiThread));
+                t.IsBackground = true;
+                t.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                inputPath.Text = ex.Message;
+            }
+
+        }
+
+        private void SayHiThread()
+        {
+            for (int i = 0; i < numberOfMinutes * 6; i++)
+            {
+                Invoke(new SayHiDelegate(SayHi), i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void SayHi(int timeInterval)
+        {
+            richTextBox2.Text = Convert.ToString(timeInterval * 10);
+            for (int i = 0; i < tenantShards.Count; i++)
+            {
+                if (data[i, timeInterval] == null)
+                    tenantShards[i].BackColor = Color.Green;
+                else if (data[i, timeInterval].Equals("true", StringComparison.OrdinalIgnoreCase))
+                    tenantShards[i].BackColor = Color.Red;
+                else if (data[i, timeInterval].Equals("false", StringComparison.OrdinalIgnoreCase))
+                    tenantShards[i].BackColor = Color.Black;
+                else
+                    tenantShards[i].BackColor = Color.Green;
+            }
+        }
+    }
+    public delegate void SayHiDelegate(int minute);
+}
